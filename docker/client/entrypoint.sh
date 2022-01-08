8f3fef3072a2a48f89875f798fbfc340b766301a
Ben Timby <btimby@gmail.com>
2022-01-08 15:45:31 -0500

    Skeleton

---
+++ docker/client/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Options:
+# -T don't allocate a pty
+# -i identity file
+# -K keepalive (seconds?)
+# -y accept host key
+# -N don't run a remote command
+# -R remote port forwarding
+
+while true; do
+    echo "Starting ssh client..."
+    ssh -TNy -K 300 -i ${SSH_KEY} -R 0:${SSH_FORWARD_HOST}:${SSH_FORWARD_PORT} \
+        ${SSH_USER}@${SSH_HOST}:${SSH_PORT}
+    echo "SSH client died, restarting..."
+    sleep 3
+done
---
+++ sshd/index.js
@@ -0,0 +1,9 @@
+const ssh2 = require('ssh2')
+
+function main() {
+  console.log('Starting sshd...');
+}
+
+if (require.main === module) {
+  main();
+}

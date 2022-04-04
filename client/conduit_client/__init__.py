87d6e593ac06633d6e1fb87e7828f617da1b3073
Ben Timby <btimby@gmail.com>
2022-04-04 01:41:15 -0400

    Allow importing from main package.

--- client/conduit_client/__init__.py
+++ client/conduit_client/__init__.py
@@ -1,4 +1,5 @@
 from conduit_client import dns, ssh
+from conduit_client.server import SSHManagerClient, SSHManagerServer
 
 
-__all__ = ['dns', 'ssh']
+__all__ = ['dns', 'ssh', 'SSHManagerClient', 'SSHManagerServer']
